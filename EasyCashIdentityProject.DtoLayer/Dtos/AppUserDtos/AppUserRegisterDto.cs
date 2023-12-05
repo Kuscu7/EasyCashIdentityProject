07f71cd08a7d52a28376aefd45094c735be4d9b5
Kuscu7 <berkcankuscu5@gmail.com>
2023-12-05 13:19:02 +0300

    Dto Layer and AppUserRegisterDto Class

---
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        // hata mesajları burada gösterilebilir. ve çeşiti arttıralabilir.
+        //[Required(ErrorMessage ="Ad Alanı zorunludur.")]
+        //[Display(Name="İsim:")]
+        //[MaxLength(30,ErrorMessage ="En fazla 30 karakter girilebilir")]
+        public string Name{ get; set; }
+        public string SurName{ get; set; }
+        public string Email{ get; set; }
+        public string Username{ get; set; }
+        public string Password{ get; set; }
+        public string ConfirmPassword{ get; set; }
+    }
+}
